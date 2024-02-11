ed6ae9955e1f33aff561404d0f1272900fa751cf
Malhar1243 <99002776+Malhar1243@users.noreply.github.com>
2024-02-11 18:41:52 +0530

    Add files via upload

---
+++ JAVASCRIPT.js
@@ -0,0 +1,15 @@
+document.getElementById('generate').addEventListener('click', function() {
+  var length = document.getElementById('length').value;
+  var password = generatePassword(length);
+  document.getElementById('password').value = password;
+});
+
+function generatePassword(length) {
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  return password;
+}
